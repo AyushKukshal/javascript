bb481a373757f8de988a56b711914861557c6c1c
Ayush Kukshal <ayushkukshal@Ayushs-MacBook-Air.local>
2025-10-04 23:22:02 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountid = 123456;
+let accountname = "John Doe";
+var accountbalance = 1000.50;
+accountcity="New York"; 
+
+accountname = "Jane Smith"; // Updating the account name
+console.log("Account ID:", accountid);
+console.log("Account Name:", accountname);
